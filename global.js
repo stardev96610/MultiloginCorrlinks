3aad87774d3a0b4610d1f4b857c0ae39be562958
stardev96610 <stardev96610@gmail.com>
2021-12-02 20:41:41 +0800

    port 8000

--- global.js
+++ global.js
@@ -35,7 +35,7 @@ app.get('/webhook', function(req, res) {
     }
 });
 
-let server = app.listen(8080, function() {
+let server = app.listen(8000, function() {
     var host = server.address().address;
     var port = server.address().port
     console.log("Example app listening at http://%s:%s", host, port);
